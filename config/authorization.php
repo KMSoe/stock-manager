f40db3afd11716ed51f138cf42523a22ed31532a
Kaung Myat Soe <kaungmyatsoe.m192@gmail.com>
2025-03-23 17:48:56 +0630

    modify: roles authorization config

--- config/authorization.php
+++ config/authorization.php
@@ -1,14 +1,14 @@
 <?php
 
 return [
-    'Admin' => [
+    'admin' => [
         'can' => [
             'view_users', 'create_users', 'edit_users',
             'view_items', 'create_items', 'edit_items', 'delete_items',
             'view_transactions', 'create_transactions', 'delete_transaction',
         ],
     ],
-    'User'  => [
+    'user'  => [
         'can' => ['view_items', 'view_transactions', 'create_transactions', 'delete_transaction'],
     ],
 ];
